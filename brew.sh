b374ee9e454590a59bd20a662619af54fe83d938
John Slivka <john.slivka@beatport.com>
2018-01-08 11:51:27 -0700

    add initial homebrew config commit

---
+++ brew.sh
@@ -0,0 +1,143 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Check for Homebrew,
+# Install if we don't have it
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade --all
+
+# Install GNU core utilities (those that come with OS X are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed --with-default-names
+# Install Bash 4.
+brew install bash
+brew tap homebrew/versions
+brew install bash-completion2
+# We installed the new shell, now we have to activate it
+echo "Adding the newly installed shell to the list of allowed shells"
+# Prompts for password
+sudo bash -c 'echo /usr/local/bin/bash >> /etc/shells'
+# Change to the new shell, prompts for password
+chsh -s /usr/local/bin/bash
+
+# Install `wget` with IRI support.
+brew install wget --with-iri
+
+# Install Python
+brew install python
+brew install python3
+
+# Install ruby-build and rbenv
+brew install ruby-build
+brew install rbenv
+LINE='eval "$(rbenv init -)"'
+grep -q "$LINE" ~/.extra || echo "$LINE" >> ~/.extra
+
+# Install more recent versions of some OS X tools.
+brew install vim --override-system-vi
+brew install homebrew/dupes/grep
+brew install homebrew/dupes/openssh
+brew install homebrew/dupes/screen
+brew install homebrew/php/php55 --with-gmp
+
+# Install other useful binaries.
+brew install ack
+brew install dark-mode
+brew install git
+brew install git-lfs
+brew install git-flow
+brew install git-extras
+brew install hub
+brew install imagemagick --with-webp
+brew install lua
+brew install lynx
+brew install p7zip
+brew install pigz
+brew install pv
+brew install rename
+brew install rhino
+brew install speedtest_cli
+brew install ssh-copy-id
+brew install tree
+brew install webkit2png
+brew install zopfli
+brew install pkg-config libffi
+brew install pandoc
+
+# Lxml and Libxslt
+brew install libxml2
+brew install libxslt
+brew link libxml2 --force
+brew link libxslt --force
+
+# Install Heroku
+brew install heroku-toolbelt
+heroku update
+
+# Install Cask
+brew install caskroom/cask/brew-cask
+brew tap caskroom/versions
+
+# Core casks
+# https://www.alfredapp.com/
+brew cask install --appdir="/Applications" alfred
+brew cask install --appdir="~/Applications" iterm2
+brew cask install --appdir="~/Applications" java
+brew cask install --appdir="~/Applications" xquartz
+
+# Development tool casks
+brew cask install --appdir="/Applications" sublime-text
+brew cask install --appdir="/Applications" virtualbox
+brew cask install --appdir="/Applications" vagrant
+brew cask install --appdir="/Applications" macdown
+brew cask install --appdir="/Applications" pycharm
+brew cask install google-cloud-sdk
+brew cask install minikube
+brew cask install spectacle
+
+# misc casks
+brew cask install --appdir="/Applications" google-chrome
+brew cask install --appdir="/Applications" firefox
+brew cask install --appdir="/Applications" 1password
+
+# Install Docker, additional development tool deps
+brew install docker
+brew install docker-machine
+brew install docker-machine-driver-xhyve
+brew install hub
+brew install ansible
+brew install awscli
+brew install jq
+brew install kubernetes-cli
+brew install packer
+brew install transcrypt
+brew install watch
+
+# Install developer friendly quick look plugins; see https://github.com/sindresorhus/quick-look-plugins
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package
+
+# Remove outdated versions from the cellar.
+brew cleanup
